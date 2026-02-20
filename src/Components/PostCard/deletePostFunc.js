39373f4b95daa07247d8dc94f613fe77859ded6a
abdalladiaa <abdallazx009@gmail.com>
2026-02-20 07:27:15 +0200

    update

---
+++ src/Components/PostCard/deletePostFunc.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { headersObjData } from "../../Helper/HeadersObj";
+
+export async function deletePostFunc(postId) {
+  try {
+    const response = await axios.delete(
+      `https://route-posts.routemisr.com/posts/${postId}`,
+      headersObjData(),
+    );
+    console.log(response);
+    return response;
+  } catch (err) {
+    console.log(err, "DeletePost func");
+  }
+}
+
